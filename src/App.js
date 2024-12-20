d5ef006ba2d7865bae8d7a72c3e0c0e6da60d9e6
otsoweckstrom <97otso@gmail.com>
2024-12-20 20:55:52 +0200

    fix order of sections

--- src/App.js
+++ src/App.js
@@ -7,85 +7,84 @@ import Contact from './components/Contact'
 import './App.css'
 
 const App = () => {
-  const homeRef = useRef(null)
-  const aboutRef = useRef(null)
-  const servicesRef = useRef(null)
-  const contactRef = useRef(null)
-  const [activeSection, setActiveSection] = useState('home')
-  const [showNavbar, setShowNavbar] = useState(false)
+	const homeRef = useRef(null)
+	const aboutRef = useRef(null)
+	const servicesRef = useRef(null)
+	const contactRef = useRef(null)
+	const [activeSection, setActiveSection] = useState('home')
+	const [showNavbar, setShowNavbar] = useState(false)
 
-  const scrollToSection = (section) => {
-    switch (section) {
-      case 'home':
-        homeRef.current.scrollIntoView({ behavior: 'smooth' })
-        break
-      case 'about':
-        aboutRef.current.scrollIntoView({ behavior: 'smooth' })
-        break
-      case 'services':
-        servicesRef.current.scrollIntoView({ behavior: 'smooth' })
-        break
-      case 'contact':
-        contactRef.current.scrollIntoView({ behavior: 'smooth' })
-        break
-      default:
-        break
-    }
-  }
+	const scrollToSection = (section) => {
+		switch (section) {
+			case 'home':
+				homeRef.current.scrollIntoView({ behavior: 'smooth' })
+				break
+			case 'about':
+				aboutRef.current.scrollIntoView({ behavior: 'smooth' })
+				break
+			case 'services':
+				servicesRef.current.scrollIntoView({ behavior: 'smooth' })
+				break
+			case 'contact':
+				contactRef.current.scrollIntoView({ behavior: 'smooth' })
+				break
+			default:
+				break
+		}
+	}
 
-  const handleScroll = () => {
-    const sections = [
-      { name: 'home', ref: homeRef },
-      { name: 'about', ref: aboutRef },
-      { name: 'services', ref: servicesRef },
-      { name: 'contact', ref: contactRef },
-    ]
-    const scrollPosition = window.scrollY + window.innerHeight / 2
+	const handleScroll = () => {
+		const sections = [
+			{ name: 'home', ref: homeRef },
+			{ name: 'about', ref: aboutRef },
+			{ name: 'services', ref: servicesRef },
+			{ name: 'contact', ref: contactRef },
+		]
+		const scrollPosition = window.scrollY + window.innerHeight / 2
 
-    for (let section of sections) {
-      const element = section.ref.current
-      if (
-        element.offsetTop <= scrollPosition &&
-        element.offsetTop + element.clientHeight > scrollPosition
-      ) {
-        setActiveSection(section.name)
-        break
-      }
-    }
-    setShowNavbar(window.scrollY > 0)
-  }
+		for (let section of sections) {
+			const element = section.ref.current
+			if (
+				element.offsetTop <= scrollPosition &&
+				element.offsetTop + element.clientHeight > scrollPosition
+			) {
+				setActiveSection(section.name)
+				break
+			}
+		}
+		setShowNavbar(window.scrollY > 0)
+	}
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll)
+		return () => {
+			window.removeEventListener('scroll', handleScroll)
+		}
+	}, [])
 
-  return (
-    <div className='App'>
-      <Navbar
-        scrollToSection={scrollToSection}
-        activeSection={activeSection}
-        showNavbar={showNavbar}
-      />
-      <div>
-        <section ref={homeRef} id='home'>
-          <Home />
-        </section>
-        <section ref={servicesRef} id='services'>
-          <About />
-        </section>
-        <section ref={aboutRef} id='about'>
-          <Projects />
-        </section>
-
-        <section ref={contactRef} id='contact'>
-          <Contact />
-        </section>
-      </div>
-    </div>
-  )
+	return (
+		<div className="App">
+			<Navbar
+				scrollToSection={scrollToSection}
+				activeSection={activeSection}
+				showNavbar={showNavbar}
+			/>
+			<div>
+				<section ref={homeRef} id="home">
+					<Home />
+				</section>
+				<section ref={servicesRef} id="services">
+					<Projects />
+				</section>
+				<section ref={aboutRef} id="about">
+					<About />
+				</section>
+				<section ref={contactRef} id="contact">
+					<Contact />
+				</section>
+			</div>
+		</div>
+	)
 }
 
 export default App
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,61 +3,64 @@ import '../styles/projects.css'
 import '../constants/index.js'
 
 const Projects = () => {
-  const { projects } = require('../constants/index.js')
-  return (
-    <div className='projects'>
-      <h3 className='title'>PROJECTS</h3>
-      <h2 className='subtitle'>
-        I've also worked on multiple professional projects but unfortunately I
-        can't share those here
-        <p className='line'>
-          I'm more than happy to talk about them though if you're interested!
-        </p>
-        <p className='line'> Here are some of my favorite personal projects.</p>
-      </h2>
+	const { projects } = require('../constants/index.js')
+	return (
+		<div className="projects">
+			<h3 className="title">PROJECTS</h3>
+			<h2 className="subtitle">
+				<p className="line">
+					While I can’t share some of my professional work here, I’d
+					be happy to discuss it if you're curious!
+				</p>
+				<p className="line">
+					Take a look at some of my favorite personal projects below.
+				</p>
+			</h2>
 
-      <div className='projects-container'>
-        {projects.map((project, index) => (
-          <div key={index} className='project'>
-            <h3 className='project-title'>{project.name}</h3>
-            <p className='project-description'>{project.description}</p>
-            <div className='project-image-container'>
-              <img
-                src={project.image}
-                alt={project.name}
-                className='project-image'
-              />
-            </div>
-            <div className='tags'>
-              {project.tags.map((tag, index) => (
-                <div key={index} className='tag'>
-                  <p>{tag.name}</p>
-                </div>
-              ))}
-            </div>
-            <div className='links-container'>
-              <a
-                className='project-source'
-                href={project.source_code_link}
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                {project.source_code_link}
-              </a>
-              <a
-                className='project-source'
-                href={project.youtube_link}
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                {project.youtube_link}
-              </a>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+			<div className="projects-container">
+				{projects.map((project, index) => (
+					<div key={index} className="project">
+						<h3 className="project-title">{project.name}</h3>
+						<p className="project-description">
+							{project.description}
+						</p>
+						<div className="project-image-container">
+							<img
+								src={project.image}
+								alt={project.name}
+								className="project-image"
+							/>
+						</div>
+						<div className="tags">
+							{project.tags.map((tag, index) => (
+								<div key={index} className="tag">
+									<p>{tag.name}</p>
+								</div>
+							))}
+						</div>
+						<div className="links-container">
+							<a
+								className="project-source"
+								href={project.source_code_link}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								{project.source_code_link}
+							</a>
+							<a
+								className="project-source"
+								href={project.youtube_link}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								{project.youtube_link}
+							</a>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default Projects
