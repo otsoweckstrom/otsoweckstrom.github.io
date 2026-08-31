bd43c0f09a379878076bddecf7950dab4fecfecd
Otso Weckström <42272286+otsoweckstrom@users.noreply.github.com>
2026-08-31 21:40:25 +0300

    Remove unused 'experiences' so the CI build (warnings-as-errors) passes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,6 @@ import '../constants/index.js'
 import Experience from './ExperienceTimeline.jsx'
 
 const About = () => {
-  const { experiences } = require('../constants/index.js')
   return (
     <div className='about'>
       <Experience />
